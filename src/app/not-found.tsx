b5ff4631a362306056192bf6c28b71a954375e4d
Mmeso1 <anisiubammesoma8@gmail.com>
2025-05-16 17:16:43 +0100

    done with not foun page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen text-center px-4">
+      <img
+        src="/DashboardIcons/tele.svg"
+        alt="Simbi with a confused face"
+        className="w-40 h-40 mb-6"
+      />
+
+      <h1 className="text-2xl md:text-3xl font-semibold mb-2 text-red-500">
+        404: Ambition detected.
+      </h1>
+      <p className="text-lg text-gray-600 mb-8">
+        But this page doesn’t exist (yet).
+      </p>
+
+      <Link
+        href="/dashboard"
+        className="inline-block px-6 py-3 bg-gray-800 text-white rounded-xl hover:bg-gray-800 transition"
+      >
+        Back to reality
+      </Link>
+    </main>
+  );
+}
